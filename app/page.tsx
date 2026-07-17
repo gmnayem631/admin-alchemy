f331ceab98ee6d515e762b21629b08bb6f5cbd3f
gmnayem631 <gmnayem631@gmail.com>
2026-07-17 09:41:08 +0600

    feat: add navigation bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <>
       <h1 className="text-2xl">Dashboard</h1>
-      <Button variant={"link"}>Click Me</Button>
     </>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../img/logo.png";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Navbar = () => {
+  return (
+    <div className="bg-primary text-white py-2 px-5 flex justify-between items-center">
+      <Link href={"/"}>
+        <Image src={logo} alt="Admin Alchemy" width={56}></Image>
+      </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger render={<Button className={"cursor-pointer"} />}>
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback className={"text-black font-semibold"}>
+              CN
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <Link href={"/profile"}>Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href={"/auth"}>Logout</Link>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default Navbar;
