1b827b0ec27fa79e345749605ffc7fd867be8846
gmnayem631 <gmnayem631@gmail.com>
2026-07-17 23:21:45 +0600

    feat: implement DashboardCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import DashboardCard from "@/components/dashboard/DashboardCard";
+import { Newspaper } from "lucide-react";
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Dashboard</h1>
+      <div className="flex flex-col justify-between gap-5 mb-5"></div>
+      <DashboardCard
+        title="Posts"
+        count={100}
+        icon={<Newspaper className="text-slate-500" size={72} />}
+      ></DashboardCard>
     </>
   );
 }
