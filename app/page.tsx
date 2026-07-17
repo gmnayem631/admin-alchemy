26d2661242330adeb6d51e10295ecb9d5b7fff48
gmnayem631 <gmnayem631@gmail.com>
2026-07-17 23:26:50 +0600

    feat: make the dashboard cards dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,31 @@
 import DashboardCard from "@/components/dashboard/DashboardCard";
-import { Newspaper } from "lucide-react";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col justify-between gap-5 mb-5"></div>
-      <DashboardCard
-        title="Posts"
-        count={100}
-        icon={<Newspaper className="text-slate-500" size={72} />}
-      ></DashboardCard>
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+        <DashboardCard
+          title="Posts"
+          count={100}
+          icon={<Newspaper className="text-slate-500" size={64} />}
+        ></DashboardCard>
+        <DashboardCard
+          title="Categories"
+          count={12}
+          icon={<Folder className="text-slate-500" size={64} />}
+        ></DashboardCard>
+        <DashboardCard
+          title="Users"
+          count={750}
+          icon={<User className="text-slate-500" size={64} />}
+        ></DashboardCard>
+        <DashboardCard
+          title="Comments"
+          count={1200}
+          icon={<MessageCircle className="text-slate-500" size={64} />}
+        ></DashboardCard>
+      </div>
     </>
   );
 }
