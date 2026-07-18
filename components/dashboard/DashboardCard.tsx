0aff2a76a6f93bb69ee0150f6fdcad577c44f86d
gmnayem631 <gmnayem631@gmail.com>
2026-07-18 10:35:10 +0600

    feat: add posts table with edit route, and change the favicon

--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -1,12 +1,4 @@
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { LucideIcon } from "lucide-react";
 
 interface DashboardCardProps {
