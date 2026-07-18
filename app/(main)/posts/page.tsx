08ca8a8a5675b1f98c1e4140d61eb68bef933320
gmnayem631 <gmnayem631@gmail.com>
2026-07-18 14:43:26 +0600

    feat: add BackButton and PostPagination components to posts page

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,9 +1,13 @@
+import BackButton from "@/components/BackButton";
+import PostPagination from "@/components/posts/PostPagination";
 import PostsTable from "@/components/posts/PostsTable";
 
 const page = () => {
   return (
     <>
+      <BackButton text="Go Back" link="/" />
       <PostsTable />
+      <PostPagination />
     </>
   );
 };
